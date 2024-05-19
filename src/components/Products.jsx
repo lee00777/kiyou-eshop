5cc60f8b35300163ffd8c6da654c0e7655265e12
Gyuyoung <gyuyoung.lee@statcan.gc.ca>
2024-05-19 13:13:33 -0400

    Fixed filter issue

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,40 +12,18 @@ import { useEffect } from 'react';
 export default function Products({category}) {
   const { productsQuery: { isPending, error, data:products}} = useProducts();
   const { filterQuery:{ data : filters } } = useFilters(category);
+
   const [ clickedFilter , setClickedFilter ] = useState('All');
-  const [ data, setData ]= useState();
+  const [ filterFlag, setFilterFlag ] = useState(false);
 
   const handleFilterSelection = (filter)=>{
     setClickedFilter(filter)
-    // 데이터 filter 해주기
+    setFilterFlag(true);
   }
   useEffect(()=>{
     setClickedFilter('All')
   },[category])
 
-  // const test = (filter) => {
-  //   if(products){
-  //     if(clickedFilter === "All"){
-  //       setData(products.filter(item => item.category.includes(category)))
-  //     }else{
-  //       setData(
-  //         products
-  //         .filter(item => item.category.includes(filter))
-  //         .filter(item => item.category.includes(clickedFilter))
-  //       )
-  //     }
-    
-   
-  // }
-
-  // useEffect(()=>{
-  //   test()
-  // },[clickedFilter])
-
-  // const filteredProducts = products && products
-  // .filter(item => item.category.includes(category))
-  // .filter(item => item.category.includes(clickedFilter));
-  // console.log('하씨밤바:', filteredProducts)
 
   return (
     <div className='body-wrapper max-w-screen-2xl mx-auto'>
@@ -71,29 +49,25 @@ export default function Products({category}) {
 
       <ul className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8 p-16 mt-4 my-12  h-auto'>
         {/* 맨 처음 페이지에 모든 제품들 다 보여주기  */}
-        {category && category.length === 0 && products && products.map(product => <ProductCard key={product.id} product={product}/>)}
+        {console.log("category:", category, category.length)}
+        { category.length === 0 && products && products.map(product => <ProductCard key={product.id} product={product}/>)}
         {/* skirts, dresses, pants처럼 특정카테고리 클릭하면 보여주기 */}
-        {/* {category && category.length >=1 && products && clickedFilter === 'All'? 
+        {category && category.length >=1 && products && clickedFilter === 'All' && 
           products
             .filter(item => item.category.includes(category))
             .map(item => (
-              <ProductCard key={item.id} product={item} specificCategory={category}/>
-            )) :
-          filteredProducts.length === 0 ? (
-            <p className='col-start-2 col-end-4 text-center text-sm'>No products match the filter</p>
-          ) : (
-            filteredProducts.map(item => (
-              <ProductCard key={item.id} product={item} specificCategory={category}/>
+              <ProductCard key={item.id} product={item} />
             ))
-          )
         }
-       */}
-        {category && category.length >=1 && products && 
-          products.filter(item => item.category.includes(category)).map(item => (
-              <ProductCard key={item.id} product={item} specificCategory={category}/>
-            )) 
+        {category && category.length >=1 && products && filterFlag && 
+          products
+            .filter(item => item.category.includes(category))
+            .filter(item => item.category.includes(clickedFilter))
+            .map(item => (
+              <ProductCard key={item.id} product={item} />
+            ))
         }
-      
+
       </ul>
     </div>
   );

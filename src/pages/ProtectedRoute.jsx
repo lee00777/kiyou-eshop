1153943857b9dc8287547afaa9a73f88266680ed
Gyuyoung <gyuyoung.lee@statcan.gc.ca>
2024-06-15 16:11:35 -0400

    Fixed a protected route component issue

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useAuthContext } from "../contexts/AuthContext";
 import { useNavigate } from 'react-router-dom';
 
@@ -6,18 +6,12 @@ export default function ProtectedRoute({ children, requiredAdmin }) {
   const { user, loading } = useAuthContext();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!user || (requiredAdmin && !user.isAdmin)) {
-      // 사용자가 권한이 없는 경우 또는 관리자 권한이 필요하지만 관리자가 아닌 경우
-      alert('권한이 없습니다. 홈페이지로 이동합니다.');
-      navigate('/');
-    }
-  }, [user, requiredAdmin, navigate]);
-
   if (loading) {
     return <div>Loading...</div>;
   }
-
-  // 권한이 있으면 children을 그대로 반환
+  if (!user || (requiredAdmin && !user.isAdmin)) {
+    alert('You have no access to the page. Redirecting to the home page.');
+    navigate('/');
+  }
   return children;
 }
